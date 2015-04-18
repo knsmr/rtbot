121e3e50e62b749c11246cacb8bf24a3d47b040d
Ken Nishimura <ken97531@gmail.com>
2015-04-18 14:20:46 +0900

    use test account and a different port

--- rtbot.go
+++ rtbot.go
@@ -55,7 +55,7 @@ func init() {
 }
 
 func main() {
-	tweetBuffer := NewTwitterBuffer("conf.json")
+	tweetBuffer := NewTwitterBuffer("conf.test-account")
 
 	// Check and store the current stats for the first time
 	articles := withinDays(fetchArticles(3), Config.days)
@@ -313,7 +313,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func startServer() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8082", nil)
 }
 
 // article template
